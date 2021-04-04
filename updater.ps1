15c070477c6fb236f5b2a720cfbd3baddaf337e9
Rodion Kovalenko <rodion.kovalenko@npo-applications.de>
2021-04-04 20:32:53 +0200

    prod build

---
+++ updater.ps1
@@ -0,0 +1,10 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
+git restore .
+git clean -f -x
+git pull
+
+pause
